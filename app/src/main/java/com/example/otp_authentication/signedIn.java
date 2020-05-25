f4def45fdd869b525cece04c15d0a50bcf56839e
Tanesh Gupta <taneshgupta2211@gmail.com>
2020-05-25 16:16:07 +0530

    implement main activity

---
+++ app/src/main/java/com/example/otp_authentication/signedIn.java
@@ -0,0 +1,14 @@
+package com.example.otp_authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signedIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signed_in);
+    }
+}
