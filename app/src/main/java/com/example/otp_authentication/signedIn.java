82a1aa9f7b4a87f6c762bf7b5f261e0ed76e13c0
Tanesh Gupta <taneshgupta2211@gmail.com>
2020-05-27 14:37:41 +0530

    implement signedIn activity

--- app/src/main/java/com/example/otp_authentication/signedIn.java
+++ app/src/main/java/com/example/otp_authentication/signedIn.java
@@ -14,6 +14,42 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class signedIn extends AppCompatActivity {
 
+    Button btnSignOut;
+    TextView txtPhone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signed_in);
+
+        findViews();
+
+        setPhoneNumber();
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(signedIn.this,MainActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private void findViews() {
+        txtPhone=findViewById(R.id.txtPhone);
+        btnSignOut=findViewById(R.id.btnSignOut);
+    }
+
+    private void setPhoneNumber(){
+        FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
+        try {
+            txtPhone.setText(user.getPhoneNumber());
+        }catch (Exception e){
+            Toast.makeText(this,"Phone number not found", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
+
 
 
